a52458c8495a1c778d45aa15c6f6b5070c6e734f
Mohammed-Nazar <x3raqe@gmail.com>
2023-08-15 15:09:30 +0300

    .

--- src/components/vans/vans.jsx
+++ src/components/vans/vans.jsx
@@ -2,44 +2,49 @@ import { useState, useEffect } from "react";
 import Nav from "../Nav/Nav";
 import Footer from "../footer/footer";
 import "../../server";
-import "./vans.css"
+import "./vans.css";
 
 export default function Vans() {
   const [vans, setVans] = useState([]);
+  const [filter, setfilter] = useState("");
+
+
   useEffect(() => {
     fetch("/api/vans")
       .then((res) => res.json())
       .then((data) => setVans(data.vans));
   }, []);
 
-  const vanEl = vans.map(van => (
+  const vanEl = vans.map((van) => (
     <div key={van.id} className="van-card">
-      <img src={van.imageUrl}/>
+      <img src={van.imageUrl} />
       <div className="van-info">
-        <h3>{van.name}</h3>
-        <p>
-          ${van.price}
-          <span>/day</span>
-        </p>
+        <div className="van-price">
+          <h3>{van.name}</h3>
+          <p>
+            ${van.price}
+            <span>/day</span>
+          </p>
+        </div>
       </div>
-      <i className={`van-type ${van.type} selected`}>{van.type}</i>{" "}
+      <button className={`van-type ${van.type} selected`}>{van.type[0].toUpperCase() + van.type.slice(1)}</button>{" "}
     </div>
   ));
   return (
     <>
       <Nav AcVa={true} />
-      <div className="vans">
+      <div className="vans-title">
         <h2>Explore our van options</h2>
         <ul>
-          <li>Simple</li>
+          <li onClick={(i)=>{
+            console.log(i.target.nodeName)
+          }}>Simple</li>
           <li>Luxury</li>
           <li>Rugged</li>
           <p>Clear filters</p>
         </ul>
       </div>
-      <div className="vans-con">
-      {vanEl}
-      </div>
+      <div className="vans-con">{vanEl}</div>
       <Footer />
     </>
   );
